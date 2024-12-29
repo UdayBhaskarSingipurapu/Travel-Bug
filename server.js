b97cd347ee4e2d06ef693535bd17b9b3e5900fdb
Uday Bhaskar <s.udaybhaskar2005@gmail.com>
2024-12-29 17:58:34 +0530

    listing-validation

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const path = require('path');
 const Listing = require('./models/listings.js');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const wrapAsync = require('./utlis/wrapAsync');
+const ExpressError = require('./utlis/ExpressError');
+const { listSchema } = require('./listSchemaJoi');
 require('dotenv').config();
 
 main()
@@ -30,6 +33,16 @@ app.get('/', async (req, res) => {
     res.render("listings/index.ejs", { allListings});
 })
 
+const validateListing = (req, res, next) => {
+    const {error} = listSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map(el => el.message).join(", ");
+        throw new ExpressError(errMsg, 400);
+    } else {
+        next();
+    }
+}
+
 // --- Index Route to show all listings--- //
 app.get('/listings', async (req, res) => {
     const allListings = await Listing.find({});
@@ -42,15 +55,16 @@ app.get('/listings/add', (req, res) => {
     res.render("listings/addnew.ejs");
 })
 // post details to db
-app.post('/listings', async (req, res) => {
+app.post('/listings', validateListing, wrapAsync(async (req, res, next) => {
     // let {title, description, image, price, location, country} = req.body;
     // let list = req.body.listing;
     // const newListing = new Listing(list);
     // console.log(newListing);
+    // if(!req.body.listing) throw new ExpressError('Add relevant details', 400);
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect('/listings');
-})
+}))
 
 
 // show route to display a single listing information
@@ -69,13 +83,14 @@ app.get('/listings/:id/edit', async (req, res) => {
 })
 
 // change details in DB
-app.put('/listings/:id', async (req, res) => {
+app.put('/listings/:id', validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
+    // if(!req.body.listing) throw new ExpressError('Add relevant details', 400);
     const updatedList = (req.body.listing);
     // console.log(updatedList);
     await Listing.findByIdAndUpdate(id, {...updatedList}, {runValidators : true, new : true});
     res.redirect(`/listings/${id}`);
-})
+}))
 
 // Route to delete listings from DB
 app.delete('/listings/:id', async (req, res) => {
@@ -84,6 +99,15 @@ app.delete('/listings/:id', async (req, res) => {
     res.redirect('/listings');
 })
 
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+})
+
+app.use((err, req, res, next) => {
+    const {message = "Something went wrong", statusCode = 500} = err;
+    // res.status(statusCode).send(message);
+    res.render("error.ejs", { message, statusCode});
+})
 
 app.listen(5050, () => {
     console.log("app listening on port 5050")
